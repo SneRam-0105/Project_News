4158c7a3c3e17ef4b2a756cd734210db9b4e731b
Sneha <133229057+SneRam-0105@users.noreply.github.com>
2024-11-06 22:42:54 +0200

    Uploading Base Folder structure with HTMLand CSS for Homepage

---
+++ frontend/components/Footer/footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+    return (
+        <>
+            <div className="bottom-section">
+                <p>Ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>914749847</p>
+                <p>Watch the news on Youtube</p>
+                <div className="social-icons">
+                    <a href="#"><img src="instagram.png" alt="Instagram" /></a>
+                    <a href="#"><img src="facebook.png" alt="Facebook" /></a>
+                    <a href="#"><img src="twitter.png" alt="Twitter" /></a>
+                </div>
+                <p>Follow us</p>
+            </div>
+        </>
+    )
+};
+export default Footer;
