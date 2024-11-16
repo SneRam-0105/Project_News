d7b500b65fb7fe24d9f118cbbddb20d0871db136
Sneha <133229057+SneRam-0105@users.noreply.github.com>
2024-11-16 20:20:45 +0200

    Changes made in Articles and deleted duplicate components

--- frontend/src/components/Weather/widget.jsx
+++ frontend/src/components/Weather/widget.jsx
@@ -17,7 +17,17 @@ function Widget() {
                     <p>Taliquet elementum. Consectetur adipiscing elit</p>
                 </div>
                 <div className="card-image">
-                    <img src="your-image-url.jpg" alt="Card Image">
+                    <img src="https://media.istockphoto.com/id/1369150014/vector/breaking-news-with-world-map-background-vector.jpg?s=612x612&w=0&k=20&c=9pR2-nDBhb7cOvvZU_VdgkMmPJXrBQ4rB1AkTXxRIKM=" alt="Card Image">
+                    </img>
+                </div>
+            </div>
+            <div className="section-header">Watch the news on youtube</div>
+            <div className="card">
+                <div className="card-content">
+                    <p>Taliquet elementum. Consectetur adipiscing elit</p>
+                </div>
+                <div className="card-image">
+                    <img src="https://media.istockphoto.com/id/1369150014/vector/breaking-news-with-world-map-background-vector.jpg?s=612x612&w=0&k=20&c=9pR2-nDBhb7cOvvZU_VdgkMmPJXrBQ4rB1AkTXxRIKM=" alt="Card Image">
                     </img>
                 </div>
             </div>
