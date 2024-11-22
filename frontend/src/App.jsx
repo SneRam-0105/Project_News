a6c7ca2808422a12058e56e7b2e2dd57b4f14f1d
Sneha <133229057+SneRam-0105@users.noreply.github.com>
2024-11-22 12:01:38 +0200

    Installed MUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-
+import * as React from 'react';
 import './App.css'
 import Header from './components/Header/header'
 import Widget from './components/Weather/widget'
 import Footer from './components/Footer/footer'
 import Articles from './components/Articles/It_articles'
+import Button from '@mui/material/Button';
+
 
 function App() {
   return (
@@ -13,20 +15,33 @@ function App() {
       <div className="main"><Articles /></div>
       <div className="aside"><Widget /></div>
       <div className="footer"><Footer /></div>
+      <Button variant="contained">Hello world</Button>;
     </div>
-
-
-
-
-
-
-
-
-
-
-
-
   )
+
+
 };
 
-export default App
+
+
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
