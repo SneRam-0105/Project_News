e6f44765813a142cc383a07123891921a679ab36
fizzaishfaq110 <s2400788@edu.bc.fi>
2024-12-08 22:54:34 +0200

    Fix: card formatting linked to index of card

--- frontend/src/pages/Business_Page.jsx
+++ frontend/src/pages/Business_Page.jsx
@@ -1,37 +1,50 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Grid, Container, CircularProgress } from "@mui/material";
 import axios from "axios";
 import ArticleCard from "../components/Article/ArticleCard";
 
 const Business_Page = () => {
-
     const [articles, setArticles] = useState([]);
-    const [loading, setLoading] = useState([true]);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         axios
             .get("http://localhost:5002/Homepage/Business")
             .then((response) => {
                 setArticles(response.data.articles);
-                console.log(response.data.articles)
+                console.log(response.data.articles);
                 setLoading(false);
             })
             .catch((error) => {
                 console.error("Error fetching data:", error);
                 setLoading(false);
             });
-
     }, []);
 
     return (
-        <div>
-            {
-                loading ? (
-                    <p>Loading...</p>
-                ) : (articles.map((news) => (
-                    <ArticleCard key={news.id} {...news} />
-                )))}
-
-        </div>
+        <Container>
+            {loading ? (
+                <CircularProgress />
+            ) : (
+                <Grid container spacing={2}>
+                    {articles.map((news, index) => (
+                        <Grid
+                            item
+                            key={news.id}
+                            xs={12}
+                            sm={index % 6 === 0 ? 9 : 3}
+                            md={index % 6 === 0 ? 9 : 3}
+                            sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                            }}
+                        >
+                            <ArticleCard {...news} />
+                        </Grid>
+                    ))}
+                </Grid>
+            )}
+        </Container>
     );
 };
 
