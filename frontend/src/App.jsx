e8f04b752bfed79ec22575c74668a438d7fc0b62
sbakter <sbakter@gmail.com>
2024-12-10 09:23:10 +0200

    footer update2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,9 @@ import Home_Page from "./pages/Home_Page";
 import IT_Page from "./pages/IT_Page";
 import Business_Page from "./pages/Business_Page";
 import Edu_Page from "./pages/Edu_Page";
-//import WeatherApp from './pages/Weather_Page'
+// import WeatherApp from './pages/Weather_Page'
 import RootLayout from "./Routes/RootLayout";
+import Footer from "./components/Footer/footer"; // Import Footer component
 
 const App = () => {
   const router = createBrowserRouter(
@@ -24,7 +25,13 @@ const App = () => {
       </Route>
     )
   );
-  return <RouterProvider router={router} />;
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+      <Footer /> {/* Include Footer here */}
+    </div>
+  );
 };
 
 export default App;
