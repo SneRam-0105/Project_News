6a836f20caeaa035cacc9cc4d51f8541134ba1d7
BhaskarDeb2000 <bhaskardeb120@gmail.com>
2024-12-19 18:21:34 +0200

    chore: DarkMode to Header and footer :tada:

--- frontend/src/Routes/RootLayout.jsx
+++ frontend/src/Routes/RootLayout.jsx
@@ -2,14 +2,14 @@ import Footer from "../components/Footer/footer";
 import Header from "../components/Header/header";
 import { Outlet } from "react-router-dom";
 
-const RootLayout = () => {
-	return (
-		<div className="Header">
-			<Header />
-			<Outlet />
-			<Footer />
-		</div>
-	);
+const RootLayout = ({ isDarkMode, handleThemeChange }) => {
+  return (
+    <div className={`RootLayout ${isDarkMode ? 'dark' : 'light'}`}>
+      <Header isDarkMode={isDarkMode} handleThemeChange={handleThemeChange} />
+      <Outlet />
+      <Footer isDarkMode={isDarkMode} />
+    </div>
+  );
 };
 
 export default RootLayout;
